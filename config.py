6ae8202d8fdc28320071ebdeaed4133c8cf6c204
Voldemaryyy <140020652+Voldemaryyy@users.noreply.github.com>
2026-05-22 02:55:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,16 @@
+import os
 # ======================================================
 # config.py — конфігурація бота
 # ======================================================
 
 # --- Telegram ---
-BOT_TOKEN = "YOUR_BOT_TOKEN"
-# Залиш порожнім [], щоб дозволити всім
+BOT_TOKEN = os.getenv("BOT_TOKEN")# Залиш порожнім [], щоб дозволити всім
 ALLOWED_USERS: list[int] = []
 
 # --- WordPress ---
 WP_URL = "http://pervomaisk-9.raiosvita.mk.ua"
 WP_USER = "redaktor"
-WP_PASSWORD = "YOUR_WP_PASSWORD"
-# --- Категорії WordPress ---
+WP_PASSWORD = os.getenv("WP_PASSWORD")# --- Категорії WordPress ---
 WP_CATEGORIES = {
     "НОВИНИ": 258,
     "Новини": 283,
